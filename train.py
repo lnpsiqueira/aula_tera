7700979f49c423dabd58cf6e522126c64b0aabde
Luciano Siqueira <32102950+lnpsiqueira@users.noreply.github.com>
2021-03-22 16:25:30 -0300

    Add files via upload

---
+++ train.py
@@ -0,0 +1,60 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+import pandas as pd
+import category_encoders as ce
+import joblib
+
+CAT_COLS = ["district_name", "district_region"]
+NUM_COLS = ["loan_amount", "loan_duration", "acc_age", "client_age", "district_avg_salary"]
+TARGET = "bad_payer"
+
+def load_data():
+    df = pd.read_csv("data/dataset.csv")
+    X = df[CAT_COLS + NUM_COLS]
+    y = df[TARGET]
+    return X, y
+
+def get_model_pipeline():
+    # Check: # https://scikit-learn.org/stable/auto_examples/compose/plot_column_transformer_mixed_types.html
+    categorical_transformer = Pipeline([
+        ("TargetEncoder", ce.woe.WOEEncoder())
+    ])
+   
+    numeric_transformer = Pipeline([
+        ("Imputer", SimpleImputer(strategy="mean")),
+        ("Scaler", StandardScaler())
+    ])
+    
+    preprocessor = ColumnTransformer([
+        ("categorical", categorical_transformer, CAT_COLS),
+        ("numeric", numeric_transformer, NUM_COLS),
+    ])
+
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor), 
+                               ('classifier', RandomForestClassifier(n_estimators=2000, class_weight={0: 1, 1: 2}))])
+    
+    return pipeline
+
+
+def main():
+    print("[INFO] Start train script...")
+    
+    print("[INFO] Loading the data...")
+    X, y = load_data()
+    
+    print("[INFO] Training the model...")
+    pipeline = get_model_pipeline()
+    pipeline.fit(X, y)
+    
+    print("[INFO] Saving the model pipeline artifact...")
+    joblib.dump(pipeline, 'models/model_v0.joblib') 
+    
+    print("[INFO] train script ended")
+
+if __name__ == "__main__":
+    main()
